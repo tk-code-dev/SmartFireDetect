7b8a13d23b1ea1239850ceca7393b2b82eadd83d
tkcode <tkcode.dev@gmail.com>
2019-11-16 18:02:28 -0500

    ADD layouts

--- app/src/main/java/com/tk/code/smartfiredetect/Controller/MessageActivity.java
+++ app/src/main/java/com/tk/code/smartfiredetect/Controller/MessageActivity.java
@@ -1,16 +1,46 @@
 package com.tk.code.smartfiredetect.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.tk.code.smartfiredetect.Model.SampleData;
 import com.tk.code.smartfiredetect.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MessageActivity extends AppCompatActivity {
+
+    SampleData sampleData = new SampleData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // use this setting to improve performance
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager rLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(rLayoutManager);
+
+        // copy to ArrayList
+        final List<String> iName = new ArrayList<String>(Arrays.asList(sampleData.names));
+
+        final List<Boolean> iSOSs = new ArrayList<Boolean>(Arrays.asList(sampleData.isSOS));
+
+
+        // specify an adapter
+        final RecyclerView.Adapter rAdapter = new MyAdapter(iNames,  iSOSs);
+        recyclerView.setAdapter(rAdapter);
+
+
     }
 }
